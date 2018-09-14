55ee3496999816d89af2c7d3bdd8d34abcc2d10b
Tony Greeley <tony.greeley@me.com>
2018-09-14 15:36:01 -0500

    pseudocode for js added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,23 @@
+//Remaing number of guesses is already generated
+
 // Press any key to get started
+
+//Make a list of possible words
+
+//Have computer select a word to be guessed
+
+//Code to handle processing user's guess
+
+//Check user's guess against current word
+
+    //Guess is correct
+        //reveal guessed letter in current word
+
+    //Guess is incorrect
+        //add guessed letter to letters guessed and decrease guesses remainging
+        
+        //if guess is repeat of anohter previous incorrect guess letters guessed and guesses remaining are not effected
+
+ //If user wins, increase win count by 1 and start playing song and show completed guessed word at top of screen
+ 
+ //If wins or loses, reset game after last screen 
