b58ee038aa461184b0fa680a93f50c155827defb
Tony Greeley <tony.greeley@me.com>
2018-09-17 14:55:06 -0500

    Game is able to now accept user guess and compare it to the word to be guessed and determine whether or not its there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,61 @@
 //Define any possible variables
+var guessesRemaining = 13;
+var userGuess = "";
+var chosenWord = "";
+var blankSpace = "";
 
 
 // Press any key to get started
 
 //Make a list of possible words
 var phrases = [
-    "Vanjie",
-    "reading",
-    "runway",
-    "drag race",
-    "love",
-    "RuPaul",
-    "lip sync",
-    "pit crew",
-    "snatch game",
-    "Michelle Visage",
-    "sashay",
-    "shantay"
+    "VANJIE",
+    "READING",
+    "RUNWAY",
+    "DRAG RACE",
+    "LOVE",
+    "RUPAUL",
+    "LIP SYNC",
+    "PIT CREW",
+    "SNATCH GAME",
+    "MICHELLE VISAGE",
+    "SASHAY",
+    "SHANTAY"
 ]
 //Have computer select a word to be guessed and display empty lines
-var chosenWord = phrases[Math.floor(Math.random() * phrases.length)];
+chosenWord = phrases[Math.floor(Math.random() * phrases.length)];
 console.log(chosenWord);
 var currentWordP = document.getElementById("currentWord");
-var blankSpace = "";
-console.log(blankSpace);
 for(var i = 0; i < chosenWord.length; i++){
     var j = chosenWord[i];
     if (j === " ") {
         blankSpace += " ";
     }else{
     blankSpace += "_";
-    console.log(blankSpace);
     }
-} 
-currentWordP.textContent = blankSpace;
+}
 
+currentWordP.textContent = blankSpace;
+console.log(blankSpace);
+
+var numberRemainingP = document.getElementById("numberRemaining");
+numberRemainingP.textContent = guessesRemaining;
 
 //Code to handle processing user's guess
+document.onkeyup = function(event) {
+    userGuess = event.key.toUpperCase();
+    console.log(userGuess);
 
+    for(var i = 0; i < chosenWord.length; i++){
+        if(chosenWord[i] === userGuess){
+            console.log("true");
+            blankSpace[i] = userGuess;
+            console.log(blankSpace[i]);
+        }
+    }
+
+    
+}
 //Check user's guess against current word
 
     //Guess is correct
